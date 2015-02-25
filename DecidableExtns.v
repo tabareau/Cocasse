d016e5f6eb3d11966a2b60757377d71a44401992
Éric Tanter <etanter@gmail.com>
2015-02-25 06:30:03 -0300

    clean up some coqdoc comments

--- DecidableExtns.v
+++ DecidableExtns.v
@@ -1,12 +1,6 @@
-(** printing {| $\{|$*)
-(** printing |} $|\}$*)
-
-(* begin hide *)
 Require Import Bool List Le.
 
 Set Implicit Arguments.
-
-(* end hide *)
 
 (** The [Decidable] type class: *)
 
@@ -15,20 +9,15 @@ Class Decidable (A : Prop) :=
 
 Arguments dec A {_}.
 
-
 (** ** Basic boolean reflection *)
 
 Instance Decidable_bool (t : bool) : Decidable (Is_true t).
-(* begin show *)
 destruct t. 
 - left. exact I. 
 - right. exact id.
 Defined. 
-(* end show *)
 
-(** ** Boolean reflection with proven relation to a proposition 
- %\label{app:bool_refl}%
-*)
+(** ** Boolean reflection with proven relation to a proposition *)
 
 Class Decidable_relate (P : Prop) := {
   Decidable_witness : bool;
@@ -38,28 +27,22 @@ Class Decidable_relate (P : Prop) := {
 
 Instance Dec_relate_Decidable P (HP: Decidable_relate P) : 
   Decidable P.
-(* begin show *)
 destruct HP as [witness spec]. destruct witness.
 - left. exact (proj1 spec eq_refl).
 - right. intro p. pose (proj2 spec p). inversion e.
 Defined.
-(* end show *)
 
 Definition Decidable_Dec_relate P (HP: Decidable P) :
   Decidable_relate P.
-(* begin show *)
 case HP; intro p.
 - refine {| Decidable_witness := true |}.  split; auto.
 - refine {| Decidable_witness := false |}. split; auto.
   intro e; inversion e.
 Defined.
-(* end show *)
 
-(* begin hide *)
 (* That instance is good to show the equivalence between the styles of decidability, 
 but introduces infinite loops in type class resolution *)
 Reset Decidable_Dec_relate.
-(* end hide *)
 
 (** ** Decidable equality *)
 
@@ -71,12 +54,10 @@ Class EqDecidable (A : Type) := {
 
 (** *** Instances for [bool] *)
 
-(* begin show *)
 Instance Decidable_eq_bool : forall (x y : bool), Decidable (eq x y).
 intros. destruct x, y; try (left ;reflexivity); 
         try (right; intro H; inversion H).
 Defined.
-(* end show *)
 
 Instance EqDecidable_bool : EqDecidable bool := 
   { eq_dec := Decidable_eq_bool }.
@@ -98,12 +79,10 @@ Instance EqDecidable_nat : EqDecidable nat :=
   { eq_dec := Decidable_eq_nat }.
 
 
-
 (** *** Instances for [list] *)
 
 Instance Decidable_eq_list : forall A (HA: EqDecidable A) 
   (x y: list A), Decidable (eq x y).
-(* begin show *)
 intros A HA. induction x.
 - destruct y.
   + left; reflexivity.
@@ -118,7 +97,6 @@ intros A HA. induction x.
     * right. unfold not in *. 
       intro Hc. inversion Hc. exact (H H1).
 Defined.
-(* end show *)
 
 Instance EqDecidable_list : 
   forall A (HA: EqDecidable A), EqDecidable (list A) := 
@@ -128,7 +106,6 @@ Instance EqDecidable_list :
 
 Instance Decidable_eq_option : forall A (HA: EqDecidable A) 
   (x y: option A), Decidable (eq x y).
-(* begin show *)
 intros. destruct x; destruct y.
 - case (eq_dec a a0); intro H.
   + left. rewrite H. reflexivity.
@@ -138,7 +115,6 @@ intros. destruct x; destruct y.
 - right. unfold not. intro Hc. inversion Hc.
 - left. reflexivity.
 Defined.
-(* end show *)
 
 Instance EqDecidable_option :
   forall A (HA: EqDecidable A), EqDecidable (option A) := 
@@ -148,14 +124,12 @@ Instance EqDecidable_option :
 
 Instance Decidable_and P Q (HP : Decidable P) 
  (HQ : Decidable Q) : Decidable (P /\ Q).
-(* begin show *)
 destruct HP.
 - destruct HQ. 
   + exact (inl (conj p q)).
   + apply inr. intro H. exact (n (proj2 H)).
 - apply inr. intro H. exact (n (proj1 H)).
 Defined.
-(* end show *)
 
 Instance Decidable_or P Q (HP : Decidable P)
         (HQ : Decidable Q) : Decidable (P \/ Q).
@@ -189,7 +163,6 @@ Instance Decidable_False : Decidable False := inr id.
 (** ** Some arithmetic *)
 
 Instance Decidable_le_nat : forall (x y : nat), Decidable (x <= y).
-(* begin show *)
 induction x.
 - destruct y.
  + left ;reflexivity.
@@ -199,26 +172,8 @@ induction x.
   + case (IHx y). intro H. left. exact (le_n_S _ _ H).
     intro H; right. intro. apply H. exact (le_S_n _ _ H0). 
 Defined.
-(* end show *)
 
 (** ** Decidability of proven properties *)
 
 Instance Decidable_proven (P : Prop) (ev :  P):  Decidable P :=
   inl ev.
-
-
-(* Instance Decidable_and_right (P Q : Prop) *)
-(*   (ev: P) (HQ : Decidable Q) : *)
-(*     Decidable (P /\ Q). *)
-(* case HQ; intro H. *)
-(* - left. exact (conj ev H). *)
-(* - right. intro. destruct H0. exact (H H1). *)
-(* Defined. *)
-
-(* Instance Decidable_and_left (P Q : Prop) *)
-(*   (HP : Decidable P) (ev: Q)  : *)
-(*     Decidable (P /\ Q). *)
-(* case HP; intro H. *)
-(* - left. exact (conj H ev). *)
-(* - right. intro. destruct H0. exact (H H0). *)
-(* Defined. *)
